fa430e9d17dfc74420206029714e4f0c8112fe71
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2025-10-04 19:33:43 +0930

    format to black

--- sra_repo/filestore.py
+++ sra_repo/filestore.py
@@ -1,4 +1,3 @@
-
 import re
 import pathlib
 import shutil
@@ -10,15 +9,15 @@ from flufl.lock import Lock, TimeOutError
 from sra_repo.utils import cexit, cerr, check_gzip_file
 
 
-re_sraid = re.compile(r'(\D+)(.+)')
+re_sraid = re.compile(r"(\D+)(.+)")
 
-proper_prefixes = ['ERR', 'SRR', 'SRS']
+proper_prefixes = ["ERR", "SRR", "SRS"]
 
 
 @dataclass
 class SRA_Info:
-    """ make validation info as a class instead of just a dictionary to
-        enforce its structure
+    """make validation info as a class instead of just a dictionary to
+    enforce its structure
     """
 
     sra_id: str
@@ -69,7 +68,7 @@ class SRA_Info:
             md5sums=self.md5sums,
             metadata=self.metadata,
         )
-        with open(path, 'w') as f:
+        with open(path, "w") as f:
             json.dump(d, f)
 
     @classmethod
@@ -80,25 +79,36 @@ class SRA_Info:
 
 
 class SRAFileStorage(object):
-    """ SRA FASTQ file repository system
-
-    """
+    """SRA FASTQ file repository system"""
 
     SRA_Info = SRA_Info
 
-    dir_secure_mode = (stat.S_IRUSR | stat.S_IXUSR |
-                       stat.S_IRGRP | stat.S_IXGRP |
-                       stat.S_IROTH | stat.S_IXOTH)
+    dir_secure_mode = (
+        stat.S_IRUSR
+        | stat.S_IXUSR
+        | stat.S_IRGRP
+        | stat.S_IXGRP
+        | stat.S_IROTH
+        | stat.S_IXOTH
+    )
 
-    dir_edit_mode = (stat.S_IWUSR | stat.S_IWGRP |
-                     stat.S_IRUSR | stat.S_IXUSR |
-                     stat.S_IRGRP | stat.S_IXGRP |
-                     stat.S_IROTH | stat.S_IXOTH)
+    dir_edit_mode = (
+        stat.S_IWUSR
+        | stat.S_IWGRP
+        | stat.S_IRUSR
+        | stat.S_IXUSR
+        | stat.S_IRGRP
+        | stat.S_IXGRP
+        | stat.S_IROTH
+        | stat.S_IXOTH
+    )
 
     def __init__(self, storage_root_path: pathlib.Path):
         self.__storage_root_path__ = pathlib.Path(storage_root_path)
-        if not (self.__storage_root_path__ / '.sra-repo-db').is_file():
-            cexit(f'ERROR: root fs {self.__storage_root_path__} is not a SRA repo storage')
+        if not (self.__storage_root_path__ / ".sra-repo-db").is_file():
+            cexit(
+                f"ERROR: root fs {self.__storage_root_path__} is not a SRA repo storage"
+            )
 
     def store(
         self,
@@ -113,17 +123,17 @@ class SRAFileStorage(object):
         # cheap sanity checks
 
         # 1 - check extension
-        if not all([path.name.lower().endswith('.fastq.gz') for path in fullpaths]):
-            raise ValueError('not all fullpaths have .fastq.gz extension')
+        if not all([path.name.lower().endswith(".fastq.gz") for path in fullpaths]):
+            raise ValueError("not all fullpaths have .fastq.gz extension")
 
         # 2 - check name
-        if not all('_' in path.name for path in fullpaths):
-            raise ValueError('not all fullpaths have proper paired fastq filename')
+        if not all("_" in path.name for path in fullpaths):
+            raise ValueError("not all fullpaths have proper paired fastq filename")
 
         # 3 - crosscheck again for file sizes
         for p in fullpaths:
             if p.stat().st_size != info.get_size(p.name):
-                raise ValueError('file {p} has different file size from SRA info')
+                raise ValueError("file {p} has different file size from SRA info")
 
         store_dir = self.get_dirpath(sra_id)
         store_dir.mkdir(parents=True, exist_ok=True)
@@ -138,13 +148,10 @@ class SRAFileStorage(object):
                     self.store_fastq(path, store_dir=store_dir, use_move=use_move)
 
                 # save the read and base counts
-                self.__store_validation_info(
-                    store_dir,
-                    info
-                )
+                self.__store_validation_info(store_dir, info)
 
         except TimeOutError:
-            raise ValueError(f'timeout lock error for SRA {sra_id}')
+            raise ValueError(f"timeout lock error for SRA {sra_id}")
 
         finally:
             store_dir.chmod(self.dir_secure_mode)
@@ -186,7 +193,7 @@ class SRAFileStorage(object):
          - filesizes and md5sum of all fastq files
         """
 
-        info_file = store_dir / 'info.json'
+        info_file = store_dir / "info.json"
         unlink_if_exists(info_file)
         info.save(info_file)
         info_file.chmod(stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
@@ -210,7 +217,7 @@ class SRAFileStorage(object):
 
     def get_validation_info(self, sra_id: str):
         store_dir = self.get_dirpath(sra_id)
-        info_file = store_dir / 'info.json'
+        info_file = store_dir / "info.json"
         return SRA_Info.load(info_file)
 
     def link(
@@ -221,12 +228,12 @@ class SRAFileStorage(object):
         flat: bool = True,
         add_dir_prefix: bool = False,
     ):
-        """ create a symbolic link from source fastq file to outdir, return the path(s)
-            to all fastq read files """
+        """create a symbolic link from source fastq file to outdir, return the path(s)
+        to all fastq read files"""
 
         store_dir = self.get_dirpath(sraid)
         if not self.check(store_dir=store_dir):
-            raise ValueError(f'ERR: store dir {store_dir} does not exist!')
+            raise ValueError(f"ERR: store dir {store_dir} does not exist!")
 
         files = []
         if type(outdir) == str:
@@ -236,13 +243,13 @@ class SRAFileStorage(object):
             # create a symlink of files in outdir
             for a_file in store_dir.iterdir():
                 # a_file is in absolute path
-                if not a_file.name.endswith('.fastq.gz'):
+                if not a_file.name.endswith(".fastq.gz"):
                     continue
                 target_link = outdir / a_file.parts[-1]
                 if not dryrun:
                     target_link.symlink_to(a_file)
                 else:
-                    cerr(f' {target_link} -> {a_file}')
+                    cerr(f" {target_link} -> {a_file}")
                 files.append(a_file.parts[-1])
 
         else:
@@ -251,25 +258,25 @@ class SRAFileStorage(object):
             if not dryrun:
                 target_link.symlink_to(store_dir)
             else:
-                cerr(f' {target_link} -> {store_dir}')
+                cerr(f" {target_link} -> {store_dir}")
             for a_file in target_link.iterdir():
-                files.append()
+                files.append(f"{a_file.parts[-2]}/{a_file.parts[-1]}")
 
         if add_dir_prefix:
-            prefix = str(outdir) + '/'
-            files = [(prefix + f) for f in files]
+            prefix = str(outdir)
+            files = [f"{prefix}/{f}" for f in files]
 
         return files
 
     def list(self, pattern: str | None = None):
-        """ provide unsorted list of all available SRA IDs """
+        """provide unsorted list of all available SRA IDs"""
 
         sra_ids = []
 
         # walk across 1st layer
         for dir_1 in self.__storage_root_path__.iterdir():
 
-            if dir_1.name == '.sra-repo-db':
+            if dir_1.name == ".sra-repo-db":
                 continue
 
             # walk across 2nd layer
@@ -287,15 +294,15 @@ class SRAFileStorage(object):
     def delete(self, sra_id: str):
         store_dir = self.get_dirpath(sra_id)
         if not store_dir.is_dir():
-            raise ValueError(f'SRA ID: {sra_id} does not exist in DB')
+            raise ValueError(f"SRA ID: {sra_id} does not exist in DB")
         shutil.rmdir(store_dir)
 
     def get_dirpath(self, sra_id: str, check: bool = False):
-        """ return a Path """
+        """return a Path"""
 
         prefix, suffix = re_sraid.match(sra_id).groups()
         if prefix not in proper_prefixes:
-            cexit(f'ERR: prefix {prefix} is not recognized.')
+            cexit(f"ERR: prefix {prefix} is not recognized.")
 
         dir_1 = suffix[:2]
         dir_2 = suffix[2:4]
@@ -303,19 +310,22 @@ class SRAFileStorage(object):
         path = self.__storage_root_path__ / dir_1 / dir_2 / sra_id
         if check:
             if not path.is_dir():
-                raise ValueError(f'SRA {sra_id} does not exist!')
+                raise ValueError(f"SRA {sra_id} does not exist!")
         return path
 
     def get_read_files(self, sra_id: str):
         sra_dir = self.get_dirpath(sra_id, check=True)
-        return [a_file for a_file in sra_dir.iterdir() if a_file.name.endswith('.fastq.gz')]
+        return [
+            a_file for a_file in sra_dir.iterdir() if a_file.name.endswith(".fastq.gz")
+        ]
 
-    def check(self,
-              *,
-              sra_id: str | None = None,
-              store_dir: str | None = None,
-              verify: bool = False,
-              throw_exc: bool = True
+    def check(
+        self,
+        *,
+        sra_id: str | None = None,
+        store_dir: str | None = None,
+        verify: bool = False,
+        throw_exc: bool = True,
     ):
 
         if sra_id:
@@ -324,14 +334,16 @@ class SRAFileStorage(object):
         if store_dir:
             try:
                 if not store_dir.is_dir():
-                    raise ValueError(f'SRA {sra_id} does not exist!')
+                    raise ValueError(f"SRA {sra_id} does not exist!")
                 if len(list(store_dir.iterdir())) == 0:
-                    return ValueError(f'SRA {sra_id} does not have any files!')
+                    return ValueError(f"SRA {sra_id} does not have any files!")
                 if verify:
                     for a_file in store_dir.iterdir():
-                        cerr(f' - verifying {a_file}')
+                        cerr(f" - verifying {a_file}")
                         if not check_gzip_file(a_file):
-                            raise ValueError(f'SRA {sra_id} with file {a_file} is not verified!')
+                            raise ValueError(
+                                f"SRA {sra_id} with file {a_file} is not verified!"
+                            )
             except:
                 if throw_exc:
                     raise
@@ -342,7 +354,7 @@ class SRAFileStorage(object):
         return False
 
     def get_lockfile(self, store_dir: str | pathlib.Path):
-        return (self.__storage_root_path__ / '.lock' / store_dir.name).as_posix()
+        return (self.__storage_root_path__ / ".lock" / store_dir.name).as_posix()
 
 
 def unlink_if_exists(path: pathlib.Path):
@@ -350,5 +362,6 @@ def unlink_if_exists(path: pathlib.Path):
         # this file exists, need to remove it first
         path.chmod(stat.S_IWUSR | stat.S_IWGRP)
         path.unlink()
+
 
 # EOF
